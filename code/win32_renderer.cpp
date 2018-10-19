8c9a30347ad02ed4be9a482a19fbff6a3f12e144
Barret5Ocal <barretgaylor@gmail.com>
2018-10-19 16:40:57 -0700

    almost there

--- code/win32_renderer.cpp
+++ code/win32_renderer.cpp
@@ -60,28 +60,6 @@ typedef double real64;
 
 #include "memory.cpp"
 
-enum debug_draw_type 
-{
-    DEBUG_POINT,
-    DEBUG_BOX,
-    DEBUG_LINE,
-};
-
-struct debug_draw_entry
-{
-    int Type;
-    float X;
-    float Y;
-    float DimX;
-    float DimY;
-    int Color;
-};
-
-int DebugIndex = 0;
-debug_draw_entry DebugEntries[50];
-
-#define DebugEntry(Type, X, Y, DimX, DimY, Color) DebugEntries[DebugIndex++] = {Type, X, Y, DimX, DimY, Color}
-
 struct vertex
 {
     v3 Pos;
@@ -215,7 +193,8 @@ inline character_asset GetCharacter(font_asset *Font, char Character)
     return Font->Character[Character - '!']; //33
 }
 
-int BoxDebug = 6; 
+#include "debug.cpp"
+
 void DrawCharacter(GLuint ShaderProgram, v2 Position, v2 Scale)
 {
     DebugEntry(DEBUG_BOX, Position.x, Position.y, Scale.x, Scale.y, BoxDebug++);
@@ -320,26 +299,35 @@ void DrawString(GLuint ShaderProgram, font_asset *Font, char *Text, v2 Baseline)
         glGenerateMipmap(GL_TEXTURE_2D);
         
         v2 Scale = {(float)CharData.Width,(float)CharData.Height};
+        Scale /= 2; 
         
         int c_x1, c_y1, c_x2, c_y2;
         stbtt_GetCodepointBitmapBox(&FontInfo, CharData.Codepoint, Font->scale, Font->scale, &c_x1, &c_y1, &c_x2, &c_y2);
         
+        int advance,lsb;
+        stbtt_GetCodepointHMetrics(&FontInfo, CharData.Codepoint, &advance, &lsb);
+        
         //AtX -= (c_x1);
         //AtX += 18;
-        v2 Position = v2{AtX, AtY + baseline + c_y1 + c_y2};
+        v2 Position = v2{AtX, AtY + baseline + c_y1};
         DebugEntry(DEBUG_POINT, AtX, AtY, 2, 2, 0); //RED Dot
         
         DebugEntry(DEBUG_POINT, Position.x, Position.y, 2, 2, 1); // GREEN Dot
         
+        v2 TopLeft = {Position.x - CharData.Width/2, Position.y - CharData.Height/2}; // NOTE(Barret5Ocal): CharData.Width and CharData.Height might be 2 times larger than it should be 
+        DebugEntry(DEBUG_POINT, TopLeft.x, TopLeft.y, 2, 2, 2); // GREEN Dot
+        
+        float ScaleLSB = lsb  * Font->scale;
+        
+        v2 Origin = {TopLeft.x - ScaleLSB, TopLeft.y - c_y1}; // NOTE(Barret5Ocal): The scale of the characters might be to big 
+        DebugEntry(DEBUG_POINT, Origin.x, Origin.y, 2, 2, 3); // GREEN Dot
+        
         
         DrawCharacter(ShaderProgram, Position, Scale);
         
-        int advance,lsb;
-        stbtt_GetCodepointHMetrics(&FontInfo, CharData.Codepoint, &advance, &lsb);
         
         int ScaleAdvance =  advance * Font->scale;
         AtX += ScaleAdvance;
-        int ScaleLSB = lsb  * Font->scale;
         AtX += ScaleLSB;
         float KernAdvance = 0;
         char Char1 = *Char; 
@@ -408,104 +396,6 @@ GLuint CreateShaderProgram(char *VertCode, int VertSize, char *FragCode, int Fra
     }
     
     return ShaderProgram; 
-}
-
-v4 DebugColors[] = 
-{
-    {1.0f, 0.0f, 0.0f, 1.0f}, //0
-    {0.0f, 1.0f, 0.0f, 1.0f}, //1
-    {0.0f, 0.0f, 1.0f, 1.0f}, //2
-    {1.0f, 1.0f, 0.0f, 1.0f}, //3
-    {1.0f, 0.0f, 1.0f, 1.0f}, //4
-    {0.0f, 1.0f, 1.0f, 1.0f}, //5
-    
-    {1.0f, 0.0f, 0.0f, 0.5f}, //6 
-    {0.0f, 1.0f, 0.0f, 0.5f}, //7
-    {0.0f, 0.0f, 1.0f, 0.5f}, //8
-    {1.0f, 1.0f, 0.0f, 0.5f}, //9
-    {1.0f, 0.0f, 1.0f, 0.5f}, //10
-    {0.0f, 1.0f, 1.0f, 0.5f}, //11
-    
-};
-
-void DrawDebugGraphics(GLuint DebugShader)
-{
-    glUseProgram(DebugShader);
-    
-    GLuint QuadVAO;
-    GLuint VBO;
-    
-#if 1
-    vertex Vertices[] = { 
-        // Pos      // Tex
-        {{-1.0f, 1.0f, 0.0f}, {0.0f, 1.0f}},
-        {{1.0f, -1.0f, 0.0f}, {1.0f, 0.0f}},
-        {{-1.0f, -1.0f, 0.0f}, {0.0f, 0.0f}}, 
-        
-        {{-1.0f, 1.0f, 0.0f}, {0.0f, 1.0f}},
-        {{1.0f, 1.0f, 0.0f}, {1.0f, 1.0f}},
-        {{1.0f, -1.0f, 0.0f}, {1.0f, 0.0f}}
-    };
-#else 
-    vertex Vertices[] = { 
-        // Pos      // Tex
-        {{0.0f, 2.0f, 0.0f}, {0.0f, 1.0f}},
-        {{2.0f, 0.0f, 0.0f}, {1.0f, 0.0f}},
-        {{0.0f, 0.0f, 0.0f}, {0.0f, 0.0f}}, 
-        
-        {{0.0f, 2.0f, 0.0f}, {0.0f, 1.0f}},
-        {{2.0f, 2.0f, 0.0f}, {1.0f, 1.0f}},
-        {{2.0f, 0.0f, 0.0f}, {1.0f, 0.0f}}
-    };
-#endif 
-    
-    glGenVertexArrays(1, &QuadVAO);
-    glGenBuffers(1, &VBO);
-    
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
-    
-    glBindVertexArray(QuadVAO);
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (GLvoid*)0);
-    glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-    
-    glBindBuffer(GL_ARRAY_BUFFER, 0);  
-    glBindVertexArray(QuadVAO);
-    
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);  
-    
-    for(int32 Index = 0;
-        Index <= DebugIndex;
-        ++Index)
-    {
-        debug_draw_entry Entry = DebugEntries[Index];
-        
-        GLuint WorldLocation = glGetUniformLocation(DebugShader, "World");
-        GLuint ProjectionLocation = glGetUniformLocation(DebugShader, "Projection");
-        GLuint ColorLocation = glGetUniformLocation(DebugShader, "InColor");
-        
-        m4 Ortho;
-        gb_mat4_ortho3d(&Ortho, 0, ScreenWidth, ScreenHeight, 0, -1, 1);
-        
-        m4 World; 
-        gb_mat4_identity(&World);
-        gb_mat4_translate(&World, {Entry.X, Entry.Y, 0.0f});
-        m4 ScaleM;
-        gb_mat4_scale(&ScaleM, {Entry.DimX, Entry.DimY, 0.0f});
-        World = World * ScaleM;
-        
-        glUniformMatrix4fv(WorldLocation, 1, GL_FALSE, &World.e[0]);
-        glUniformMatrix4fv(ProjectionLocation, 1, GL_FALSE, &Ortho.e[0]);
-        glUniform4f(ColorLocation, DebugColors[Entry.Color].x, DebugColors[Entry.Color].y, DebugColors[Entry.Color].z,DebugColors[Entry.Color].w);
-        
-        glDrawArrays(GL_TRIANGLES, 0, 6);
-        
-    }
-    
-    
 }
 
 LRESULT CALLBACK
